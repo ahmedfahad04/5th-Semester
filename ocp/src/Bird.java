8b33d3c8888d8a2965dae8782be04367dbf9b7b9
ahmedfahad04 <bsse1204@iit.du.ac.bd>
2022-12-12 08:28:14 +0600

    OCP updated

--- ocp/src/Bird.java
+++ ocp/src/Bird.java
@@ -1,5 +1,14 @@
-public class Bird extends ItFlys{
+public class Bird implements Flys {
     public Bird() {
         System.out.println("New Bird obj created...");
     }
+
+    @Override
+    public void fly() {
+        System.out.println("It Flys");
+    }
+
+    // i can either implement the core Fly interface
+    // I can extend the ItFly class that implement the Fly interface
+    // what's the difference?
 }
