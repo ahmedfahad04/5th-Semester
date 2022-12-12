35af505a926f2c97cc6d644ebe0e0bba03977a56
ahmedfahad04 <bsse1204@iit.du.ac.bd>
2022-12-12 22:37:06 +0600

    Assignment 1 added

---
+++ assignment_1/assignment_1/src/Main.java
@@ -0,0 +1,100 @@
+//  Explanation:  We need to make interface because the relation between rectangle might be an 'is-a'
+//  relation for Square, but they are not actually an "is substitution of" relation between them.
+//  We could call them replaceable when both of them have same methods or behaviors, but here,
+//  we observe a contrast in case of their height, width.
+//
+//  Therefore, we should create an abstraction for the behaviors that are common and expected to
+//  contain each of its implementation. That's why we have added the common functionality in
+//  an Interface and other's functionalities are class specific.
+//
+//  Thus using Interface, we can resolve the LSP Violation.
+
+interface Shape {
+
+    double calculateArea();
+    void setTopLeft(double x, double y);
+    void draw();
+
+}
+
+class Square implements Shape {
+
+    double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    void setSize(double s) {
+        this.side = s;
+    }
+
+    @Override
+    public double calculateArea() {
+        System.out.print("Square Area: ");
+        return side * side;
+    }
+
+    @Override
+    public void setTopLeft(double x, double y) {
+        // implementation
+    }
+
+    @Override
+    public void draw() {
+        System.out.print("Square - ");
+    }
+}
+
+class Rectangle implements Shape {
+    double width;
+    double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public void setSize(double w, double h) {
+        this.width = w;
+        this.height = h;
+    }
+
+    @Override
+    public double calculateArea() {
+        System.out.print("Rectangle Area: ");
+        return width * height;
+    }
+
+    @Override
+    public void setTopLeft(double x, double y) {
+        // implementation
+    }
+
+    @Override
+    public void draw() {
+        // implementation
+    }
+}
+
+class AreaCalculator {
+    public void calculateArea(Shape s) {
+        System.out.println(s.calculateArea());
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(10, 20);
+        Square square = new Square(20);
+
+        rectangle.setSize(10, 25);
+        square.setSize(25);
+
+        AreaCalculator areaCalculator = new AreaCalculator();
+        areaCalculator.calculateArea(rectangle);
+        areaCalculator.calculateArea(square);
+
+    }
+}
